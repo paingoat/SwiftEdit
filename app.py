f6775a93233bb056a16f840abef27bfc2370a740
Quan Nguyen Anh <nganhquanvn@gmail.com>
2026-06-01 20:19:46 +0700

    update port for gradio

--- app.py
+++ app.py
@@ -127,4 +127,4 @@ with gr.Blocks(title="SwiftEdit", theme=gr.themes.Soft()) as demo:
 
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    demo.launch(server_name="0.0.0.0", server_port=7860, share=True)
